2b201472534877b807048879bb132d3373335f23
Paschoal Diniz <paschoal.diniz@gmail.com>
2026-03-15 12:48:06 -0300

    feat: configure expense stack screen titles

--- app/(tabs)/(expenses)/_layout.tsx
+++ app/(tabs)/(expenses)/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from 'expo-router';
 
 export default function ExpensesLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Despesas', headerShown: false }} />
+      <Stack.Screen name="new" options={{ title: 'Nova Despesa' }} />
+      <Stack.Screen name="[id]" options={{ title: 'Editar Despesa' }} />
+    </Stack>
+  );
 }
