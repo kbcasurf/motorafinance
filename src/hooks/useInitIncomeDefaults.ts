9554189b8d248ae423f762a7a5f66529bf00d564
Paschoal Diniz <paschoal.diniz@gmail.com>
2026-03-15 11:37:13 -0300

    feat: sync income smart defaults from DB on app load

---
+++ src/hooks/useInitIncomeDefaults.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { getLastIncome } from '../db/queries/income';
+import { useIncomeStore } from '../stores/useIncomeStore';
+
+export function useInitIncomeDefaults() {
+  const { setLastPlatform, setLastOdoEnd } = useIncomeStore();
+
+  useEffect(() => {
+    getLastIncome().then((last) => {
+      if (!last) return;
+      setLastPlatform(last.platform);
+      if (last.odoEnd != null) {
+        setLastOdoEnd(last.odoEnd);
+      }
+    });
+  }, [setLastPlatform, setLastOdoEnd]);
+}
