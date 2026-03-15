7f86aa4026b8712d548dfa5cb08b34ad5f9e989b
Paschoal Diniz <paschoal.diniz@gmail.com>
2026-03-15 11:37:10 -0300

    feat: configure income stack screen titles

--- app/(tabs)/(income)/_layout.tsx
+++ app/(tabs)/(income)/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from 'expo-router';
 
 export default function IncomeLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Receitas', headerShown: false }} />
+      <Stack.Screen name="new" options={{ title: 'Nova Receita' }} />
+      <Stack.Screen name="[id]" options={{ title: 'Editar Receita' }} />
+    </Stack>
+  );
 }
